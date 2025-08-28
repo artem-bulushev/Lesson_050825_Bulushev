1125de369282f8066623b56c100a0b5f845bc307
artem-bulushev <bulushiev93@mail.ru>
2025-08-28 17:55:14 +0300

    Изменения

---
+++ Assets/Code/Bullet.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Code
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public sealed class Bullet : MonoBehaviour
+    {
+        [SerializeField] private float _damage = 1.0f;
+        [SerializeField] private float _lifeTime = 7.0f;
+
+        private Rigidbody _rigidbody;
+        private float _force = 3.0f;
+
+        public bool IsActive { get; private set; }
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _force = Random.Range(3.0f, 47.0f);
+            //_projectorHelper = new BulletProjectorHelper(_bulletHoles);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            bool tryRicochet = TryRicochet();
+            if (tryRicochet == false)
+            {
+                Destroy(gameObject);
+            }
+
+            if (other.collider.TryGetComponent(out HealthController health))
+            {
+                if (health.CanTakeDamage(_damage))
+                {
+                    if (tryRicochet == false)
+                    {
+                        ContactPoint contact = other.contacts[0];
+                        //_projectorHelper.CreateBulletHole(contact.point, contact.normal, other.transform);
+                    }
+                    return;
+                }
+
+                if (other.collider.TryGetComponent(out Rigidbody rigidbody) == false)
+                {
+                    rigidbody = other.collider.gameObject.AddComponent<Rigidbody>();
+                }
+
+                rigidbody.AddForce(_rigidbody.velocity * _force, ForceMode.Impulse);
+            }
+        }
+
+        private bool TryRicochet()
+        {
+            if (Random.Range(0.0f, 1.0f) < 0.5f)
+            {
+                return false;
+            }
+
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 1.0f))
+            {
+                Vector3 reflect = Vector3.Reflect(ray.direction, hit.normal);
+                transform.rotation = Quaternion.LookRotation(reflect);
+                _force /= 2.0f;
+                _rigidbody.AddForce(_rigidbody.velocity * _force, ForceMode.Impulse);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Sleep()
+        {
+            _rigidbody.Sleep();
+            gameObject.SetActive(false);
+            IsActive = false;
+        }
+
+        public void Run(Vector3 path, Vector3 startPosition)
+        {
+            transform.position = startPosition;
+            gameObject.SetActive(true);
+            _rigidbody.WakeUp();
+            _rigidbody.AddForce(path, ForceMode.Impulse);
+            transform.SetParent(null);
+            IsActive = true;
+            StartCoroutine(Die());
+        }
+
+        private IEnumerator Die()
+        {
+            while (_lifeTime >= 0.0f)
+            {
+                _lifeTime -= 1.0f;
+                yield return new WaitForSeconds(1.0f);
+            }
+
+            Destroy(gameObject);
+        }
+
+        //[SerializeField] private float _damage = 1.0f;
+        //[SerializeField] private float _force = 3.0f;
+
+        //private Rigidbody _rigidbody;
+
+        //public bool IsActive { get; private set; }
+
+        //public float Force
+        //{
+        //    get
+        //    {
+        //        if (_force < 0)
+        //        {
+        //            return 0;
+        //        }
+        //        return _force;
+        //    }
+
+        //    set
+        //    {
+        //        if (IsActive == false)
+        //        {
+        //            _force = 0;
+        //            return;
+        //        }
+        //        _force = value;
+        //    }
+        //}
+
+        //private void Awake()
+        //{
+        //    _rigidbody = GetComponent<Rigidbody>();
+        //}
+
+        //private void OnBecameInvisible()
+        //{
+        //    if (IsActive == false)
+        //    {
+        //        return;
+        //    }
+
+        //    Destroy(gameObject);
+        //}
+
+        //private void OnCollisionEnter(Collision other)
+        //{
+        //    Destroy(gameObject);
+
+        //    if (other.collider.TryGetComponent(out HealthController health))
+        //    {
+        //        if (health.CanTakeDamage(_damage))
+        //        {
+        //            return;
+        //        }
+
+        //        if (other.collider.TryGetComponent(out Rigidbody rigidbody) == false)
+        //        {
+        //            rigidbody = other.collider.gameObject.AddComponent<Rigidbody>();
+        //        }
+
+        //        rigidbody.AddForce(_rigidbody.velocity * Force, ForceMode.Impulse);
+        //    }
+        //}
+
+        //public void Run(Vector3 path, Vector3 startPosition)
+        //{
+        //    transform.position = startPosition;
+        //    gameObject.SetActive(true);
+        //    _rigidbody.WakeUp();
+        //    _rigidbody.AddForce(path);
+        //    IsActive = true;
+        //}
+
+        //public void Sleep()
+        //{
+        //    _rigidbody.Sleep();
+        //    gameObject.SetActive(false);
+        //    IsActive = false;
+        //}
+    }
+}
